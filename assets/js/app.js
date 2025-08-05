9dbdd751798f8087fd2167d362769867c665d019
Nurikenov <dakepromo@gmail.com>
2025-08-05 05:54:20 +0500

    add noti

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,34 +4,47 @@ console.log('Petomec загружен');
 
 
 if ('setAppBadge' in navigator) {
-  navigator.setAppBadge(3); // Покажет 3 на иконке
+    navigator.setAppBadge(3); // Покажет 3 на иконке
 }
 
 
 document.getElementById('feedBtn').addEventListener('click', async () => {
-  const action = {
-    type: 'feed',
-    timestamp: new Date().toISOString()
-  };
+    const action = {
+        type: 'feed',
+        timestamp: new Date().toISOString()
+    };
 
-  await saveAction(action);
+    await saveAction(action);
 
-  if ('serviceWorker' in navigator && 'SyncManager' in window) {
-    const reg = await navigator.serviceWorker.ready;
-    await reg.sync.register('sync-actions');
-    console.log('Background sync registered');
-  } else {
-    console.log('SyncManager not supported, sending manually');
-    sendActionsToServer(); // fallback
-  }
+    if ('serviceWorker' in navigator && 'SyncManager' in window) {
+        const reg = await navigator.serviceWorker.ready;
+        await reg.sync.register('sync-actions');
+        console.log('Background sync registered');
+    } else {
+        console.log('SyncManager not supported, sending manually');
+        sendActionsToServer(); // fallback
+    }
 });
 
 
 document.getElementById('startBtn').addEventListener('click', () => {
-  if ('vibrate' in navigator) {
-    navigator.vibrate([100, 50, 100]); // 2 коротких вибрации с паузой
-  } else {
-    console.log('no vibrate');
-    
-  }
+    if ('vibrate' in navigator) {
+        navigator.vibrate([100, 50, 100]); // 2 коротких вибрации с паузой
+    } else {
+        console.log('no vibrate');
+
+    }
+
+
+    Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+            new Notification("Petomec скучает по тебе 🐣", {
+                body: "Погладь его или накорми!",
+                icon: "/icons/icon-192.png"
+            });
+        }
+    });
 });
+
+
+
